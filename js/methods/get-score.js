bd8191b9b21adeafb23c3339600ab06fb1372e72
ARIJ <9292358@gmail.com>
2017-10-10 23:49:44 +0400

    Исправлена(упрощена) функция подсчета очков

--- js/methods/get-score.js
+++ js/methods/get-score.js
@@ -1,35 +1,32 @@
-const getScore = (answers, lives) => {
-  const Config = {
-    ANSWER_RATE: {
-      SLOW: {
-        POINTS: 50,
-        TIME_LIMIT: 30
-      },
-      NORMAL: {
-        POINTS: 100,
-        TIME_LIMIT: 20
-      },
-      FAST: {
-        POINTS: 150,
-        TIME_LIMIT: 10
-      },
+const Config = {
+  ANSWER_RATE: {
+    SLOW: {
+      POINTS: 50,
+      TIME_LIMIT: 30
     },
-    LIFE_BONUS: 50,
-    ANSWER_TIME_LIMIT: 30,
-    ERROR_LIMIT: 3,
-    QUESTIONS_NUM: 10,
-  };
+    NORMAL: {
+      POINTS: 100,
+      TIME_LIMIT: 20
+    },
+    FAST: {
+      POINTS: 150,
+      TIME_LIMIT: 10
+    },
+  },
+  LIFE_BONUS: 50,
+  QUESTIONS_NUM: 10,
+};
 
+const getScore = (answers, lives) => {
   if (answers.length < Config.QUESTIONS_NUM) {
     return -1;
   }
 
   let score = 0;
-  let errors = 0;
 
   for (const answer of answers) {
 
-    if (answer.VALUE && answer.TIME <= Config.ANSWER_TIME_LIMIT && errors <= Config.ERROR_LIMIT) {
+    if (answer.VALUE) {
 
       let answerSpeed;
       for (const k in Config.ANSWER_RATE) {
@@ -37,12 +34,9 @@ const getScore = (answers, lives) => {
           answerSpeed = k;
         }
       }
-      score += Config.ANSWER_RATE[answerSpeed].POINTS;
-    } else if (errors < Config.ERROR_LIMIT) {
-
-      errors++;
-    } else {
-      return -1;
+      if (answerSpeed) {
+        score += Config.ANSWER_RATE[answerSpeed].POINTS;
+      }
     }
   }
 
