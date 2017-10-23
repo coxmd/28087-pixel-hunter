693884b8f9d14b95371097e7013ef970e028bef7
ARIJ <9292358@gmail.com>
2017-10-23 23:55:01 +0400

    Исправлены ошибки линтера

--- js/screens/header.js
+++ js/screens/header.js
@@ -2,8 +2,8 @@ export default (data = []) => {
   let strHeader = `<header class="header">
     <div class="header__back">
       <button class="back">
-        <img src="img/arrow_left.svg" width="45" height="45" alt="Back">
-        <img src="img/logo_small.svg" width="101" height="44">
+        <img src="img/arrow_left.svg" width="45" height="45" alt="Back"/>
+        <img src="img/logo_small.svg" width="101" height="44"/>
       </button>
     </div>`;
   if(data.timer !== undefined){
@@ -12,10 +12,10 @@ export default (data = []) => {
   if(data.lives !== undefined){
     strHeader += `<div class="game__lives">
     ${new Array(3 - data.lives)
-      .fill(`<img src="img/heart__empty.svg" class="game__heart" alt="Life" width="32" height="32">`)
+      .fill(`<img src="img/heart__empty.svg" class="game__heart" alt="Life" width="32" height="32"/>`)
       .join(``)}
     ${new Array(data.lives)
-      .fill(`<img src="img/heart__full.svg" class="game__heart" alt="Life" width="32" height="32">`)
+      .fill(`<img src="img/heart__full.svg" class="game__heart" alt="Life" width="32" height="32"/>`)
       .join(``)}
     </div>
     `
--- js/screens/rules/rules-view.js
+++ js/screens/rules/rules-view.js
@@ -14,7 +14,7 @@ export default (data = screenData) => {
     <h1 class="rules__title">${data.title}</h1>
     <p class="rules__description">${data.description}</p>
     <form class="rules__form">
-      <input class="rules__input" type="text" placeholder="${data.form.placeholder}">
+      <input class="rules__input" type="text" placeholder="${data.form.placeholder}"/>
       <button class="rules__button  continue" type="submit" disabled>${data.form.button}</button>
     </form>
   </div>
